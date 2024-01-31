a0a23077ce12f449f29d4e0138c130adba7ee639
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2024-01-31 18:49:40 +0200

    update lazy imports

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,20 @@ import MainLayout from 'components/Layout/MainLayout';
 import PrivateRoute from 'helpers/PrivateRoute';
 import { useActions } from 'hooks';
 import { useAuth } from 'hooks/useAuth';
-import Clients from 'pages/Clients/Clients';
-import CompanyProfilePage from 'pages/CompanyProfilePage/CompanyProfilePage';
-import RecordLog from 'pages/RecordLog/RecordLog';
-import WorkSchedule from 'pages/WorkSchedule/WorkSchedule';
 import { lazy, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Bounce, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import { useCurrentQuery } from 'services/auth.api';
 
-const HomePage = lazy(() => import('../src/pages/Home'));
+const HomePage = lazy(() => import('pages/Home'));
+const CompanyProfilePage = lazy(() => import('pages/CompanyProfilePage'));
+const Clients = lazy(() => import('pages/Clients'));
+const WorkSchedule = lazy(() => import('pages/WorkSchedule'));
+const RecordLog = lazy(() => import('pages/RecordLog'));
+const VerifyPage = lazy(() => import('pages/Verify'));
+const ErrorPage = lazy(() => import('pages/ErrorPage'));
 const Workspace = lazy(() => import('components/Layout/UsersLayout'));
-const VerifyPage = lazy(() => import('../src/pages/Verify'));
-const ErrorPage = lazy(() => import('../src/pages/ErrorPage'));
 
 function App() {
   const { accessToken, user } = useAuth();
