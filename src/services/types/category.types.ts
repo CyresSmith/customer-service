d8f7829ed96b410c02a52557bfdeee36bc6891b6
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2024-02-23 10:38:06 +0200

    fix workingHours types

--- src/services/types/category.types.ts
+++ src/services/types/category.types.ts
@@ -1,14 +1,11 @@
+import { Activity } from 'store/company/company.types';
+
 export type CategoryType = 'employee' | 'service' | 'product' | 'activity';
 
 export type Category = {
   id: string;
   name: string;
   type: CategoryType;
-};
-
-export type Activity = {
-  id: string;
-  name: string;
 };
 
 export type CompanyCategory = {
