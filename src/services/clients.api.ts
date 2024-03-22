fcbfac5c3f3fc58be62134e4eb0f84763ff5c2b2
Andrii Kulyk <andrij.kulyk@gmail.com>
2024-03-22 09:23:50 +0200

    fix clients api

--- src/services/clients.api.ts
+++ src/services/clients.api.ts
@@ -15,74 +15,63 @@ export const clientsApi = createApi({
   tagTypes: ['clientsApi'],
 
     endpoints: builder => {
-        return ({
-            createClient: builder.mutation<Client, AddClient>({
-                query: ({ data, companyId }) => ({
-                    url: `clients/create`,
-                    method: 'POST',
-                    data,
-                    params: {companyId}
-                }),
-                invalidatesTags: ['clientsApi'],
-            }),
-
-            getAll: builder.query<Client[], unknown>({
-                query: (companyId: number) => ({
-                    url: `clients/get-all`,
-                    method: 'GET',
-                    params: {companyId}
-                }),
-            }),
-
-            getById: builder.query<Client, { companyId: number, id: number }>({
-                query: ({ companyId, id }) => ({
-                    url: `clients/${id}`,
-                    method: 'GET',
-                    params: {companyId}
-                })
-            }),
-
-            updateClient: builder.mutation<Client, UpdateClient>({
-                query: ({ companyId, id, data }) => ({
-                    url: `clients/${id}/update`,
-                    method: 'PATCH',
-                    data,
-                    params: {companyId}
-                }),
-                invalidatesTags: ['clientsApi'],
-            }),
-
-            uploadAvatar: builder.mutation<{ url: string }, UploadAvatar>({
-                query: ({ companyId, id, data }) => ({
-                    url: `clients/${id}/update/avatar`,
-                    method: 'POST',
-                    data,
-                    params: {companyId}
-                }),
-                invalidatesTags: ['clientsApi'],
-            }),
-
-            delete: builder.mutation<{message: string}, { companyId: number, id: number }>({
-                query: ({ companyId, id }) => ({
-                    url: `clients/${id}/delete`,
-                    method: 'DELETE',
-                    params: {companyId}
-                }),
-                invalidatesTags: ['clientsApi'],
-            }),
-
-      delete: builder.mutation<
-        { message: string },
-        { companyId: number; id: number }
-      >({
-        query: ({ companyId, id }) => ({
-          url: `clients/${companyId}/${id}/delete`,
-          method: 'DELETE',
+      return ({
+        createClient: builder.mutation<Client, AddClient>({
+          query: ({ data, companyId }) => ({
+            url: `clients/create`,
+            method: 'POST',
+            data,
+            params: { companyId }
+          }),
+          invalidatesTags: ['clientsApi'],
         }),
-        invalidatesTags: ['clientsApi'],
-      }),
-    };
-  },
+
+        getAll: builder.query<Client[], unknown>({
+          query: (companyId: number) => ({
+            url: `clients/get-all`,
+            method: 'GET',
+            params: { companyId }
+          }),
+        }),
+
+        getById: builder.query<Client, { companyId: number, id: number }>({
+          query: ({ companyId, id }) => ({
+            url: `clients/${id}`,
+            method: 'GET',
+            params: { companyId }
+          })
+        }),
+
+        updateClient: builder.mutation<Client, UpdateClient>({
+          query: ({ companyId, id, data }) => ({
+            url: `clients/${id}/update`,
+            method: 'PATCH',
+            data,
+            params: { companyId }
+          }),
+          invalidatesTags: ['clientsApi'],
+        }),
+
+        uploadAvatar: builder.mutation<{ url: string }, UploadAvatar>({
+          query: ({ companyId, id, data }) => ({
+            url: `clients/${id}/update/avatar`,
+            method: 'POST',
+            data,
+            params: { companyId }
+          }),
+          invalidatesTags: ['clientsApi'],
+        }),
+
+        delete: builder.mutation<{ message: string }, { companyId: number, id: number }>({
+          query: ({ companyId, id }) => ({
+            url: `clients/${id}/delete`,
+            method: 'DELETE',
+            params: { companyId }
+          }),
+          invalidatesTags: ['clientsApi'],
+        }),
+      });
+  }
 });
 
 export const {
